0e41d5af22b0d9d349f92c9c363dbf91783a941d
Yelong He <Yelong.he@hotmail.com>
2022-10-06 11:12:46 +0200

    added corsfilter

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -28,6 +28,7 @@ public class ApplicationConfig extends Application {
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.PersonResource.class);
         resources.add(rest.AddressResource.class);
+        resources.add(CorsFilter.class);
     }
 
 }
